63cd9b7f52e2b0144d6f1e1ffc14d2c3a074a25a
aiorio <alexandre.iorio@outlook.com>
2022-12-10 23:01:18 +0100

    "Ajout de quelques constructeurs et méthodes pour starter le jeu"

--- ChessGame/src/engine/board/Board.java
+++ ChessGame/src/engine/board/Board.java
@@ -5,7 +5,10 @@ import engine.piece.Piece;
 import java.util.HashMap;
 
 public class Board {
-    private final static byte SIZE = 8;
+    /**
+     * Constant of board Size
+     */
+    public final static byte SIZE = 8;
     private HashMap<Position, Piece> board;
 
 
