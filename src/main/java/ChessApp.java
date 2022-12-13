e3ae4402f4e50aecdb78dcbbf3d470a9ce1a2aaa
Anthony David <70233189+AnthonyDavid3110@users.noreply.github.com>
2022-12-13 23:59:23 +0100

    Modification schéma UML

--- src/main/java/ChessApp.java
+++ src/main/java/ChessApp.java
@@ -1,10 +1,10 @@
 import chess.ChessController;
 import chess.ChessView;
 import chess.views.gui.GUIView;
-import engine.Controller;
+import chess.engine.Controller;
 import engine.board.Board;
 
-public class Main {
+public class ChessApp {
     public static void main(String[] args) {
         // 1. Création du contrôleur pour gérer le jeu d’échec
         ChessController controller = new Controller(new Board()); // Instancier un ChessController
