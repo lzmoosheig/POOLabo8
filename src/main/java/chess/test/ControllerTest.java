fc0fa08b692d090a4a11f4cbdb3a774c81e40c4c
Léo <leo.zmoos@heig-vd.ch>
2023-01-12 17:57:10 +0100

    Added testclass with Junit

---
+++ src/main/java/chess/test/ControllerTest.java
@@ -0,0 +1,33 @@
+package chess.test;
+
+import chess.PlayerColor;
+import chess.engine.Controller;
+import chess.engine.board.Board;
+import chess.engine.board.Position;
+import chess.engine.piece.Piece;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerTest extends Controller {
+
+    /**
+     * Constructeur du Controller
+     *
+     * @param board La board que le Controller doit controller
+     */
+    public ControllerTest(Board board) {
+        super(board);
+    }
+
+    public void initializeTest(HashMap<Position, Piece> hashMap)
+    {
+        super.initializeTest(hashMap);
+    }
+
+    public boolean checkmateTest(PlayerColor player)
+    {
+        return super.checkmateTest(player);
+    }
+}
